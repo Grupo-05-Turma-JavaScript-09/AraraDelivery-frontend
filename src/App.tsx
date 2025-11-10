8f243a333ae38c04671ea84dae030fad817b664f
Flavio Serra <sonyflavio@gmail.com>
2025-11-10 12:46:32 -0300

    criação da lista de produtos

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import Navbar from './components/navbar/Navbar'
 import { AuthProvider } from './contexts/AuthContext'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
-
 import 'react-toastify/dist/ReactToastify.css'
+import ListaProdutos from './components/produto/listaproduto/ListaProduto'
 
 function App() {
 	return (
@@ -19,6 +19,7 @@ function App() {
 						<Routes>
 							<Route path="/" element={<Login />} />
 							<Route path="/home" element={<Home />} />
+							<Route path="/produtos" element={<ListaProdutos />} />
 						</Routes>
 					</div>
 					<Footer />
--- src/components/produto/listaproduto/ListaProduto.tsx
+++ src/components/produto/listaproduto/ListaProduto.tsx
@@ -1,9 +1,56 @@
+import { useEffect, useState } from "react";
+import { LinkIcon } from "@phosphor-icons/react";
+import type Produto from "../../../models/Produto";
+import { buscar } from "../../../services/Services";
+import CardProduto from "../cardproduto/CardProduto";
 
+function ListaProdutos() {
 
-function ListaProduto() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [categorias, setProdutos] = useState<Produto[]>([]);
+
+  useEffect(() => {
+    buscarProdutos();
+  }, []); // executa só uma vez
+
+  async function buscarProdutos() {
+    try {
+      setIsLoading(true);
+
+      await buscar('/produtos', setProdutos, {});
+
+    } catch (error: any) {
+      console.error("Erro ao buscar produtos:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
-    <div>ListaProduto</div>
-  )
+    <>
+      {isLoading && (
+        <div className="flex justify-center w-full my-8">
+          <LinkIcon color="#312e81" size={32} />
+        </div>
+      )}
+
+      <div className="flex justify-center w-full my-4">
+        <div className="container flex flex-col">
+          {!isLoading && categorias.length === 0 && (
+            <span className="text-3xl text-center my-8">
+              Nenhum Produto foi encontrado!
+            </span>
+          )}
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {produtos.map((produto) => (
+              <CardProduto key={produto.id} produto={produto} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default ListaProduto
+export default ListaProdutos;
